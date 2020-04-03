3cb3fa19b2bc47fd26e77ed3962d655335ed0620
Robert-Dolan <62515397+Robert-Dolan@users.noreply.github.com>
2020-04-03 12:49:48 +0100

    Update searchBySociety.php

--- WebAppProject/Search/searchBySociety.php
+++ WebAppProject/Search/searchBySociety.php
@@ -8,7 +8,7 @@
 
 
 <?php
-include 'DBController.php';
+include '/WebAppProject/database/DBController.php';
 $db_handle = new DBController();
 $nameResult = $db_handle->runQuery("SELECT Name FROM availablegroups WHERE Type = 'society'");
 ?>
