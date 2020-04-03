d0f3c00e0180209df6e79a6a992c86f4efbd5177
Logan227 <61418990+Logan227@users.noreply.github.com>
2020-04-03 13:40:17 +0100

    Update createuser.php

--- WebAppProject/Signup/createuser.php
+++ WebAppProject/Signup/createuser.php
@@ -4,14 +4,14 @@ include "/WebAppProject/Database/DBcontroller.php"; //config file in the same fo
 
 
 $userIDQuery = "SELECT user_id FROM security WHERE username = '" . $_SESSION['sUsername'] . "';";
-$UIDResults = mysqli_query($con, $userIDQuery);
+$UIDResults = mysqli_query($conn, $userIDQuery);
 $UIDrow = mysqli_fetch_array($UIDResults);
 
 $UIDResult = $UIDrow['user_id'];
 
 //echo $UIDResult;
 
-if(!mysqli_query($con, $userIDQuery))
+if(!mysqli_query($conn, $userIDQuery))
 {
     echo 'No Username';
 }
