d5d2a93ddf9af462fb051ad6e4ca07ba5d9d4525
Rob <robrandom@gmail.com>
2020-04-19 17:16:08 +0100

    Added Basic Admin PAge

--- WebAppProject/SignupForms/Login.inc.php
+++ WebAppProject/SignupForms/Login.inc.php
@@ -7,13 +7,26 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
 
     $uid = $_POST['email'];
     $password = md5($_POST['password']);
+    $sql = "SELECT * FROM admin WHERE adminusername = '{$uid}' AND adminpassword = '{$password}'";
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        if (mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            //Log in the user here
+            $_SESSION['uid'] = $row['adminusername'];
+            $_SESSION['pwd'] = $row['adminpassword'];
 
+            //enter admin homepage here
+            header("Location: ../admin/adminHome.php?login=success");
+            exit();
+
+        } 
 
     $sql = "SELECT * FROM security WHERE email = '{$uid}' OR username = '{$uid}' AND password_user = '{$password}'";
 
     //True or false if it can find a user
     $result = mysqli_query($conn, $sql);
-    if($result){
+    if ($result) {
         if (mysqli_num_rows($result) > 0) {
             $row = mysqli_fetch_assoc($result);
             //Log in the user here
@@ -23,16 +36,16 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
             //enter homepage here
             header("Location: ../Home/Home.php?login=success");
             exit();
-        } else {
-            header("Location: ./index.php?login=nouserfound");
-            exit();
         }
-    } else {
-        die("Query error");
+
+      else {
+            die("Query error");
+        }
     }
 }
+}
+
+
 
 //header("Location: ./index.php?login=error");
 //exit();
-
-?>
