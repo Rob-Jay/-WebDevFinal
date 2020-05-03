cc286d7f864f1bdee8d680f8984403087ec96b7e
Rob <robrandom@gmail.com>
2020-05-03 15:49:03 +0100

    admin page now displays users

--- WebAppProject/admin/adminHome.php
+++ WebAppProject/admin/adminHome.php
@@ -35,51 +35,18 @@ include "dbh.inc.php"
     <h2>all users</h2>
 
     <div class ="user-container">
-    <?php
-    
-          $output = "";
-          $query = mysqli_query($conn, "SELECT Photo,handle,UserID FROM profile") or die ("Could not search");
-          $count = mysqli_num_rows($query);
-
-          if($count == 0){
-              $output = "There was no search results!";
-
-          }else{
-              $i = 0;
-              while ($row = mysqli_fetch_array($query)) {
-
-                  $Name = $row ['Photo'];
-                  $handle = $row ['handle'];
-                  $id = $row['UserID'];
-
-
-                  if(($i % 3) == 0){
-                      echo ' <br> ';
-                  }
-
-
-                  $output .='<div> '.$Name.'</div>';
-                  print ("Name is \t");
-                  print ("$handle \n");
-                  print("User id is \t ");
-                  print ("$id");
-
-                  echo '  
-                    <tr>  
-                         <td>   
-                              <img src="data:image/jpeg;charset=utf-8;base64, '.base64_encode($row['Photo'] ).'" height="200" width="200"  />  
-                         </td>  
-                    </tr>  
-               ';
-                  $i = $i + 1;
-              }
-
-          }
-      
 
 
 
-      ?>
+<?php
+include "allUsers.php"
+?>
+
+
+
+
+
+
 
 
 
