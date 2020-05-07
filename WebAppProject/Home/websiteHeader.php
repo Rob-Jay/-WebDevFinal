65db8650ddab6b5c5c983db780a5902cd34a0149
andrewoc98 <60736542+andrewoc98@users.noreply.github.com>
2020-05-07 17:31:42 +0100

    Update websiteHeader.php

--- WebAppProject/Home/websiteHeader.php
+++ WebAppProject/Home/websiteHeader.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html>
 <hr>
-
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script type="text/javascript">
+	function OnLogout(){
+		$.ajax({url:"logout.inc.php",success:function(result)
+			{
+				alert(result);
+			}
+		})
+	}
 <link rel="stylesheet"  href="mysyle.css">
 <br>
 <div id = "boxes">
     <div id = "leftbox">
         <br>
+        <button onclick="OnLogout()">Log Out</button>
         <button class="btn btn1" onclick="window.location.href = 'Home.php';">Home</button>
     </div>
     <div id = "middlebox">
